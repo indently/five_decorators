6f5047dbeb7dae27b99e7c671ecd72be7bd2dc7a
Federico <federico@indently.io>
2024-03-06 19:39:11 +0100

    hello world

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-weather: str = 'cloudy'
-
-if weather == 'cloudy':
-    print('You need to bring an umbrella!')
-elif weather == 'sunny':
-    print('It is a nice day, enjoy outside')
-else:
-    print('Unknown weather...')
+# Hello, world!
